c79a3e708962a78f56e9b52a5655f8fed713057c
Vincent Sgherzi <sgherzivincent@gmail.com>
2021-04-08 01:29:53 -0700

    Initial Commit

---
+++ Design Code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Design Code
+//
+//  Created by Vince Sgherzi on 4/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Design Code/Design_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_CodeApp.swift
+//  Design Code
+//
+//  Created by Vince Sgherzi on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Design_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
